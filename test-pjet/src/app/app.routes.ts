a828ed9dbae3411e00508252c94c94b1e91380a9
Hunter Hamblin <Hamblinhunter21@gmail.com>
2026-06-12 14:54:04 -0600

    big changes

--- test-pjet/src/app/app.routes.ts
+++ test-pjet/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { World } from './world/world';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'world', pathMatch: 'full' },
+    { path: 'world', component: World }
+];
